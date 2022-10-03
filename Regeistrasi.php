66887165826e24970278322d267242ca521b2b28
SAADYMUFLIH <saadymuflih59@gmail.com>
2022-10-03 20:18:48 +0700

    registrasi

---
+++ Regeistrasi.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">User Name</label>
+                <input type="text" name="username" id="username">
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <label for="pasword">Password</label>
+                <input type="text" name="pasword" id="pasword">
+            </li>
+        </ul>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+
+
+
+    </form>
+</body>
+</html>
